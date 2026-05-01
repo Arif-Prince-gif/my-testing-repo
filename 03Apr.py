8f418a3e9b8d47356f40fca92b930b82256d3e93
Arif-Prince-gif <arif8428111558@gmail.com>
2026-05-01 20:50:54 +0530

    Add files via upload

---
+++ 03Apr.py
@@ -0,0 +1,28 @@
+a= "mouse"
+b= "cat"
+print(a+""+b)
+print(a*3)
+print(a+"!"*5)
+
+
+
+q = " I am going to build our application"
+print("own" in q)
+print("application" in q)
+
+
+a = "MohammedArif"
+
+print(a[0:8])
+print(a[8:12])
+print(a[::-1])
+
+users=["Ravi", "Kumar", "Priya"]
+for user in users:
+    print (user)
+
+
+
+e= arif@eurogriptyres.com
+r=arif@gmail.com
+if (e,r) 
---
+++ 2.py
@@ -0,0 +1,30 @@
+num = 25
+if num > 0:
+    print("Positive Number")
+elif num == 0:
+    print("Zero")
+else:
+    print("Negative Number")
+
+
+
+
+a = 10
+b = 5
+if a > b:
+    print("A is biggest")
+else:
+    print("B is biggest")
+
+c = 3
+d = 8
+e = 90
+f = 3
+if e > f:
+    print("E is bigger")
+elif c == f:
+    print("C and E both or same")
+else:
+    print("These all same")
+
+
